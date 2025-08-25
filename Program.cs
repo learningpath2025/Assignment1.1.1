8803eb41a6a55aa32ae7b914619cc84a5aa7336e
masud-github <masudmdabdullah@gmail.com>
2025-08-25 14:46:28 -0500

    initial comit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//1. Create a console application in C# to store and print personal details of a person like name, age and address on console screen. Make use of appropriate variables.
+
+string name = "Abdullah Masud";
+int age = 35;
+string address = "123 Main St, Minneapolis, Minnesota";
+
+Console.WriteLine($"User's name is: {name}\nUser's age is: {age}\nUser's address is: {address}");
